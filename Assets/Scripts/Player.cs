d3ab56ca6d1ec48c399642b89be476c98030e0df
LostReny <68451719+LostReny@users.noreply.github.com>
2020-09-19 18:26:08 -0300

    Pulo adicionado e movimentação direita/esquerda

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,7 +9,11 @@ public class Player : MonoBehaviour
 
     public float velocidade; // variavel de número decimais
 
+    public float forcaPulo;
+
     public Rigidbody2D rb; //variavel do tipo componente rigdbody 2D
+
+    bool estaPulando; //variavel do tipo verdadeiro ou falso
 
 
 
@@ -22,8 +26,45 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movimentar();
+        Pular();
+    }
+
+
+    void Movimentar()
+    {
         float movimento = Input.GetAxis("Horizontal"); // varia entre -1 e 1 
 
         rb.velocity = new Vector2(movimento * velocidade, rb.velocity.y);
+
+        if (movimento > 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 0f);
+        }
+
+        if (movimento < 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 180f);
+        }
+
+    }
+
+    void Pular()
+    {
+        if(Input.GetButtonDown("Jump") && estaPulando == false)
+        {
+            rb.AddForce(new Vector2(0f, forcaPulo), ForceMode2D.Impulse);
+            estaPulando = true;
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision) //detecta quando personagem detecta quando bate em outro colisor
+    {
+        //se colidir com o chão
+        if(collision.gameObject.layer == 8)
+        {
+            estaPulando = false;
+        }
     }
 }
